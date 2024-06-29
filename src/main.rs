9cf78c7aea4480d609f370d463bb0d54319c9e91
a2p1k02 <fl4ckyp@gmail.com>
2024-06-29 16:20:25 +0300

    Add syntax hightlight

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use std::env;
 use std::fs;
 
-extern crate colored;
-use colored::*;
+use syntect::easy::HighlightLines;
+use syntect::parsing::SyntaxSet;
+use syntect::highlighting::{ThemeSet, Style};
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 fn print_file(file_path: String) {
-    let contents = fs::read_to_string(file_path).unwrap();
-    print!("{}", String::from(contents).italic().red());
+    let contents = fs::read_to_string(file_path.clone()).unwrap();
+
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+
+    let extension = file_path.split(".").collect::<Vec<&str>>();
+
+    let syntax = ps.find_syntax_by_extension(extension[1]).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+
+    for line in LinesWithEndings::from(&contents) {
+        let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ps).unwrap();
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }       
+    // Clear the formatting
+    println!("\x1b[0m");
 }
 
 fn main() {
@@ -21,3 +38,4 @@ fn main() {
         println!("Using example: dog [file]");
     }
 }
+
